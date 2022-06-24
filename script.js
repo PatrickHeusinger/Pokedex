8d9ef90363afc769ea0287d3259d86d29b7ce5ee
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2022-06-24 09:28:53 +0200

    header trouble

---
+++ script.js
@@ -0,0 +1,54 @@
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html");
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
+
+
+
+let start = 1;
+let end = 21;
+let allPokemon = [];
+
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon?limit=400&offset=0';
+    let response = await fetch(url);
+    allPokemon = await response.json();
+    for (let i = start; i < end; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        let response = await fetch(url);
+        allPokemon[i] = await response.json();
+
+        renderPokemonInfo(i);
+    }
+    console.log(allPokemon);
+}
+
+
+
+function renderPokemonInfo(i) {
+    document.getElementById('pokemons').innerHTML += `
+
+    <div id="pokedex">
+<div class="headDivider">
+<h1 id="pokemonName">${allPokemon[i]['name'].charAt(0).toUpperCase() + ['name'].slice(1)}</h1>
+<span class="id" id="ID">#&nbsp${allPokemon[i]['id']}</span>
+</div>
+
+<img id="image" src="${allPokemon[i]['sprites']['other']['home']['front_default']}"></img><br>
+<span><b>Height</b>&nbsp:&nbsp${allPokemon[i]['height']}</span><br>
+<span><b>Weight</b>&nbsp:&nbsp${allPokemon[i]['weight']}</span><br>
+<span><b>Experience</b>&nbsp:&nbsp ${allPokemon[i]['base_experience']}</span>
+</div>
+`;
+}
